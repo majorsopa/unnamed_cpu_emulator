79df60de7232d9ecae1fbcb3a8b493b791b997c6
majorsopa <wastermed@gmail.com>
2022-07-21 16:16:50 -0500

    add stack overflow/underflow error messages

--- unnamed_cpu_lib/src/cpu_and_related/stack.rs
+++ unnamed_cpu_lib/src/cpu_and_related/stack.rs
@@ -7,12 +7,12 @@ impl Stack {
     }
     
     pub fn push(&mut self, stack_pointer: &mut u16, value: u16) {
-        self.0[*stack_pointer as usize] = value;
+        *self.0.get_mut(*stack_pointer as usize).expect("stack overflow") = value;
         *stack_pointer += 1;
     }
 
     pub fn pop(&mut self, stack_pointer: &mut u16) -> u16 {
         *stack_pointer -= 1;
-        self.0[*stack_pointer as usize]
+        *self.0.get(*stack_pointer as usize).expect("stack underflow")
     }
 }
